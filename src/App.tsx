1544d9203ea7507db4f18dc89b6d0f2304f9266e
FranciscoMontenegro97 <francisco.gm97@hotmail.com>
2024-05-09 17:00:53 -0300

    Force to refix all repositorie data

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function App() {
+  
+  return (
+    <div className=" basic-structure ">
+      <div className="t1">
+        <h1>Welcome</h1>
+      </div>
+      <div className="t2">
+        <p>En desarrollo</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
